a9afe43c729f9ffdbcc39bd22a861eacc9ba709c
dwikaananta <dwika.ananta.wiratama@gmail.com>
2022-08-18 15:38:16 +0800

    yes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,19 +3,19 @@
 @section('content')
     <div class="row">
         <div class="col-4 text-center border">
-            <h4 class="m-0">Jumlah Sepatu</h4>
+            <h4 class="m-0">Sepatu</h4>
             <h6 class="text-secondary mb-3">{{ $sepatu_min_date ? date('m-Y', strtotime($sepatu_min_date)) : '' }} s/d
                 {{ $sepatu_max_date ? date('m-Y', strtotime($sepatu_max_date)) : '' }}</h6>
             <h3 class="text-success">{{ $sepatu_total }}</h3>
         </div>
         <div class="col-4 text-center border">
-            <h4 class="m-0">Jumlah Sandal</h4>
+            <h4 class="m-0">Sandal</h4>
             <h6 class="text-secondary mb-3">{{ $sandal_min_date ? date('m-Y', strtotime($sandal_min_date)) : '' }} s/d
                 {{ $sandal_max_date ? date('m-Y', strtotime($sandal_max_date)) : '' }}</h6>
             <h3 class="text-success">{{ $sandal_total }}</h3>
         </div>
         <div class="col-4 text-center border">
-            <h4 class="m-0">Jumlah Heels</h4>
+            <h4 class="m-0">Heels</h4>
             <h6 class="text-secondary mb-3">{{ $heels_min_date ? date('m-Y', strtotime($heels_min_date)) : '' }} s/d
                 {{ $heels_max_date ? date('m-Y', strtotime($heels_max_date)) : '' }}</h6>
             <h3 class="text-success">{{ $heels_total }}</h3>
